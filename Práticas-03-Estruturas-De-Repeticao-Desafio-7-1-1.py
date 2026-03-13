d7416efee14436f7e01df42b30d285e393faf9bb
isabsousa0254-prog <isabsousa0254@gmail.com>
2026-03-12 21:05:41 -0300

    Add script for listing numbers and names

---
+++ Práticas-03-Estruturas-De-Repeticao-Desafio-7-1-1.py
@@ -0,0 +1,9 @@
+#7.1.Desafio 1: Listagem e Impressão de Dados
+
+for i in range(1,11):
+    print(i)
+
+nomes = ["Isabela", "Maria", "José", "Raimunda", "Raimundo"]
+
+for nome in nomes:
+    print(nome)
